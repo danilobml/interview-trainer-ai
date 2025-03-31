49c26594a8882d8ee9553b0b6263cba0ace5f4f6
danilobml <danbarml@gmail.com>
2025-03-31 02:38:41 +0200

    add: Interview creation screen and Agent

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,12 @@
+import Agent from "@/components/Agent";
+
+const InterviewGeneration  = () => {
+  return (
+    <>
+        <h3>Interview Generation</h3>
+        <Agent userName="Me" userId="user1" type="generate" />
+    </>
+  )
+}
+
+export default InterviewGeneration;
